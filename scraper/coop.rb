5fbf722c01312b9354d35bd32e9bef9fac19c85a
Emil Tullstedt <sakjur@gmail.com>
2011-11-20 22:19:37 +0100

    Added a (non-functional) coop ruby scraper

---
+++ scraper/coop.rb
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2011 by
+# Emil @sakjur Tullstedt <sakjur@gmail.com>
+#
+# This Ruby script requires:
+# gem mechanize
+# gem nokogirl (provided by mechanize)
+# ruby 1.9
+# libxslt-ruby
+# libxml2-ruby
+
+require "mechanize"
+
+agent = Mechanize.new
+
+$sidnr = 1
+$butiknr = 1
+
+while $sidnr <= 50 do
+	page = agent.get('http://www.coop.se/Sok/Butikssok/?q=*&bn=' + $sidnr.to_s)
+	$sidnr = $sidnr+1
+
+	page.search('.shopResult').each do |content|
+		puts $butiknr
+	
+		puts content.search('dl dt a').text
+		content.search('ul li').each do |oppettid|
+			# DETECT OPEN HOURS IN HERE
+		end
+
+		$butiknr = $butiknr+1
+
+	end
+
+end
