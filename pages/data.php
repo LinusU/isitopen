e148ee7cc5f9ede918b5bdf134bc65a19d779a6a
Linus Unnebäck <linus@folkdatorn.se>
2011-11-27 23:04:33 +0100

    Beginning of basic data-fetching per city

---
+++ pages/data.php
@@ -0,0 +1,38 @@
+<?php
+
+$city = city::select()->where("`name` = '?'", $parts[1])->one();
+
+if($city === false) {
+    return 404;
+}
+
+$venues = venue::select()->where($city)->many();
+
+echo '{"license": "Copyright, All Rights Reserved, Usage of this data without explicit permission is strictly forbidden!", "data": [';
+
+foreach($venues as $key => $venue) {
+    
+    if($key > 0) {
+        echo ",";
+    }
+    
+    $data = $venue->get_row();
+    $hours = hours::select()->where($venue)->many();
+    
+    unset($data['id']);
+    unset($data['city_id']);
+    
+    $data['hours'] = array();
+    
+    foreach($hours as $hour) {
+        $tmp = $hour->get_row();
+        unset($tmp['id']);
+        unset($tmp['venue_id']);
+        $tmp['weekday'] = explode(',', $tmp['weekday']);
+        $data['hours'][] = $tmp;
+    }
+    
+    echo json_encode($data);
+}
+
+echo ']}';
